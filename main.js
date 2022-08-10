0e13ee29a16dc80fdffaa06c96dfdb22e96568fe
emilyaikens <emily.c.aikens@gmail.com>
2022-08-10 15:39:22 -0700

    in biggerBoard function min variable set to 10

--- main.js
+++ main.js
@@ -121,6 +121,5 @@ function biggerBoard () {
     let newImages = ["images/2.png", "images/5.png", "images/7.png", "images/9.png", "images/10.png", "images/13.png", "images/15.png", "images/16.png", "images/17.png","images/18.png",
                     "images/2.png", "images/5.png", "images/7.png", "images/9.png", "images/10.png", "images/13.png", "images/15.png", "images/16.png", "images/17.png","images/18.png"];
     vars.cardImages.push(newImages);//add 10 new images to card images array
-    //update timer to 10 minutes
-    //update win conditions, matches = arraylength/2
+    min = 10; //update timer to 10 minutes
 };
