3afb795f7da834653765e7fbabaeabffe9f4cab6
emilyaikens <emily.c.aikens@gmail.com>
2022-08-12 09:31:54 -0700

    index restructure, add new card divs in manually with new-card class. CP JS from original js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let vars = {
     firstSelect: "null", //first of two clicks
     secondSelect: "null", //second of two clicks
@@ -18,10 +17,26 @@ const domSelect = {
     resetButton: document.getElementById("play-again") //reset button at bottom of page
 };
 
-if (vars.cardImages.length = 16) {
-    shuffleId(vars.cardImages);
-    setCards();
-};
+shuffleId(vars.cardImages);
+setCards();
+
+domSelect.cards.forEach(function(card) { //for each card with the card class...
+    card.addEventListener('click', function clickCard() { //on click
+        if ((vars.curCards.includes(card.firstElementChild.id) === false) && //everything under this line only happens if the card hasn't already been clicked
+                (vars.curCards.length < 2)) { //debug: now user can't click more than two cards at a time
+            card.classList.toggle('flipped');  //flip cards by adding/removing flipped clss
+            vars.clicks = vars.clicks + 1 //count clicks (clicks +1)
+            vars.curCards.push(card.firstElementChild.id); //push id of clicked card into curCards array ********
+            if (vars.clicks % 2 === 0) { //if clicks %2 set secondSelect
+                vars.secondSelect = card.firstElementChild.innerHTML; //assign image to secondSelect
+                checkMatch();
+            } else {
+                vars.firstSelect = card.firstElementChild.innerHTML; //assign image to firstSelect
+            };
+            if (vars.clicks === 1) {gameTimer()}; //if clicks === 1, start timer
+        };
+    });
+});
 
 function checkMatch () {
     if (vars.firstSelect === vars.secondSelect) { //if the images are the same...
@@ -37,7 +52,6 @@ function checkMatch () {
     };
     vars.firstSelect = "null"; // reset firstSelect and secondSelect to "null"
     vars.secondSelect = "null";
-    console.log(vars.curCards);
     setTimeout(function(){vars.curCards = []}, 1501); //clears current card array after timed flip executes
 };
 
@@ -54,7 +68,7 @@ function shuffleId(array) {
 };
 
 function setCards () {        
-    for (let i = 0; i < vars.cardImages.length; i++) {
+    for (let i = 0; i < 16; i++) {
         const img = document.createElement("img"); //create a new image div
         img.src = vars.cardImages[i]; //image source is an image from image array
         document.getElementById(i).appendChild(img); //add new image div to card parent
@@ -73,7 +87,7 @@ function gameTimer() {
             domSelect.directions.innerHTML = "Time is up, you lost";
             document.querySelector(".score-board").style.backgroundColor = "#ffcdd2";
         };
-        if (vars.matches === vars.cardImages.length / 2) { //if player get 8 matches, turn off timer and display win message
+        if (vars.matches === 8) { //if player get 8 matches, turn off timer and display win message
             clearInterval(myInterval);
             playerWin(domSelect.directions);
         };
@@ -88,57 +102,7 @@ function playerWin(element) { //DOM updates to directions
     element.classList.add("wiggle"); // add class "wiggle" to the directions 
 };
 
-function biggerBoard() {
-    document.querySelector(".card-container").classList.add("card-container-large"); //increase board size to 6x6
-    for (let i = 16; i < 36; i++) { //for loop, not starting at 0 bc need ids to start after the existing ids (16)
-        const newCard = document.createElement("div"); //create new card
-            newCard.classList.add("card"); //give new cards the card class
-        const cardBack = document.createElement("div"); //create a div
-            cardBack.classList.add("card-back"); //give the new div a card-back class
-            cardBack.setAttribute("id", i); //give the new div a new id
-        const cardFront = document.createElement("div"); //create a div
-            cardFront.classList.add("card-front"); //give the new div a card-front class
-            newCard.appendChild(cardBack); //put card back in the card div
-            newCard.appendChild(cardFront); //put the card front in the card div
-            document.querySelector(".card-container").appendChild(newCard); //put the card in the card container div
-            flipCard(newCard);
-    };
-    let newImages = ["images/2.png", "images/5.png", "images/7.png", "images/9.png", "images/10.png", "images/13.png", "images/15.png", "images/16.png", "images/17.png","images/18.png",
-                    "images/2.png", "images/5.png", "images/7.png", "images/9.png", "images/10.png", "images/13.png", "images/15.png", "images/16.png", "images/17.png","images/18.png"];
-    newImages.forEach(function(image) {vars.cardImages.push(image)});//add 10 new images to card images array
-    vars.min = 10; //update timer to 10 minutes
-    domSelect.timer.innerHTML = ("Time Left: 10:00"); //update dom timer display to 10 min
-    shuffleId(vars.cardImages);
-    setCards();
-};  
-
-domSelect.cards.forEach(function(card) { //for each card with the card class...
-    flipCard(card);
-});
-function flipCard(card) {
-    card.addEventListener('click', function clickCard() { //on click
-        if ((vars.curCards.includes(card.firstElementChild.id) === false) && //everything under this line only happens if the card hasn't already been clicked
-                (vars.curCards.length < 2)) { //debug: now user can't click more than two cards at a time
-            card.classList.toggle('flipped');  //flip cards by adding/removing flipped clss
-            vars.clicks = vars.clicks + 1 //count clicks (clicks +1)
-            vars.curCards.push(card.firstElementChild.id); //push id of clicked card into curCards array ********
-            if (vars.clicks % 2 === 0) { //if clicks %2 set secondSelect
-                vars.secondSelect = card.firstElementChild.innerHTML; //assign image to secondSelect
-                checkMatch();
-            } else {
-                vars.firstSelect = card.firstElementChild.innerHTML; //assign image to firstSelect
-            };
-            if (vars.clicks === 1) {gameTimer()}; //if clicks === 1, start timer
-        };
-    });
-};
-
-domSelect.resetButton.addEventListener("click", function() { //reload game with Play Again button
+domSelect.resetButton.addEventListener("click", function() { //reload game
     location.reload();
 });
 
-document.getElementById("harder").addEventListener("click", function () { //increase board size with Play Harder button
-    if (vars.cardImages.length < 20) {
-        biggerBoard();
-    }
-});
